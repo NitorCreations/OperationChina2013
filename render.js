6675728b1dc211112c6f8224e99d1549371ca8f6
Pasi Niemi <pasi.niemi@iki.fi>
2013-10-08 17:44:59 +0300

    Styles for pdfs

--- render.js
+++ render.js
@@ -1,12 +1,22 @@
 var page = require('webpage').create(),
     system = require('system'),
     outname, fs = require("fs");
-page.viewportSize = { width: 1920, height: 1080 };
+
+String.prototype.endsWith = function(suffix) {
+    return this.indexOf(suffix, this.length - suffix.length) !== -1;
+};
+
 if (system.args.length > 2) {
   outname=system.args[2];
 } else {
   outname=system.args[1].replace('.html', '.png');
 }
+if (outname.endsWith(".pdf")) {
+  page.viewportSize = { width: 1920 * 1.35, height: 1080 * 1.35};
+} else {
+  page.viewportSize = { width: 1920, height: 1080 };
+}
+
 page.open(system.args[1], function () {
     page.render(outname);
     phantom.exit();
